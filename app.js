857cec188ca4645ea9e1afb70ff69df9d1e0dee1
MohamedSamiMohamed <mohamed.samy9956@gmail.com>
2021-12-13 02:29:09 +0200

    initial commit with project structure

---
+++ app.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+require('express-async-errors')
+const morgan = require('morgan')
+const express = require('express');
+serverDebugger = require('debug')('server')
+const app = express();
+let cors = require('cors')
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+app.use(cors())
+const port = process.env.PORT || 3000;
+app.listen(port, () => serverDebugger(`Listening on port ${port}...`));
