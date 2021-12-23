1893848f70c04d39803b5aa23575b64f03cbf2ff
MohamedSamiMohamed <mohamed.samy9956@gmail.com>
2021-12-23 23:23:00 +0200

    activities is almost done

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const mongoose = require('mongoose');
-// const {CourseWeek} = require('./models/courseWeek')
 require('dotenv').config()
 require('express-async-errors')
 const morgan = require('morgan')
+var multer = require('multer');
 const express = require('express');
 const serverDebugger = require('debug')('server')
 const app = express();
-let cors = require('cors')
+let cors = require('cors');
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
+
 require('./start-up/db')()
 require('./start-up/routes')(app)
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 const port = process.env.PORT || 3000;
 app.listen(port, () => serverDebugger(`Listening on port ${port}...`));
-
-// async function createCourse(){
-//     let courseWeek = await new CourseWeek()
-//     console.log(courseWeek)
-// }
-
-// createCourse()
