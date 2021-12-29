55207b77c12b89bc00712b5cdadf780df5bc54ec
MohamedSamiMohamed <mohamed.samy9956@gmail.com>
2021-12-30 01:25:14 +0200

    change the response of add syllabus

--- controllers/syllabus.js
+++ controllers/syllabus.js
@@ -9,11 +9,15 @@ require("dotenv")
 exports.addSyllabus = async (req, res) => {
     let course = await Course.findOne({ _id: req.params.id })
     if (!course) return res.status(404).send("No course found with this id")
+    if (req.user.role === "instructor" && (req.user._id != course.instructor)) return res.status(403).send("you must be the instructor of the course to be able to add weeks")
     if (course.syllabus != null) return res.status(400).send("This course already has a syllabus")
     let syllabus = new Syllabus(req.body)
     try {
         await Fawn.Task().update('courses', { _id: course._id }, { syllabus: syllabus._id }).save('syllabuses', syllabus).run()
-        return res.status(201).send("This syllabus has been created for the course successfully!")
+        return res.status(201).send({
+            message: "This syllabus has been created for the course successfully!",
+            syllabus : syllabus
+        })
     }
     catch (err) {
         throw new Error(err.message)
