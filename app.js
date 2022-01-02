9a05f53f37656d2d09bc691cae49e7e52caa9405
MohamedSamiMohamed <mohamed.samy9956@gmail.com>
2022-01-03 00:59:20 +0200

    bug fic in middlewares

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const express = require('express');
 const serverDebugger = require('debug')('server')
 const app = express();
 let cors = require('cors');
+app.use(cors())
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
 require('./start-up/db').connectDB()
 require('./start-up/routes')(app)
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => serverDebugger(`Listening on port ${port}...`));
