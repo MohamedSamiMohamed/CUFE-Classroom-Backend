eca3190e427fbc6d552dfc690ec3fac477b0fd03
MohamedSamiMohamed <mohamed.samy9956@gmail.com>
2022-01-03 23:57:51 +0200

    fix a typo in userController

--- controllers/user.js
+++ controllers/user.js
@@ -6,17 +6,17 @@ exports.getMe = async (req, res) => {
     if (req.user.role === "instructor") {
         let instructor = await Instructor.findById(req.user._id).populate('courses', '-instructor -qa -weeks')
         if (!instructor) return res.status(404).send("sorry, something wrong has been occured")
-        return res.send(_.pick(instructor, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate', 'courses']))
+        return res.send(_.pick(instructor, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate', 'courses']))
     }
     else if (req.user.role === "admin") {
         let admin = await User.findById(req.user._id).populate('courses', '-instructor -qa -weeks')
         if (!admin) return res.status(404).send("sorry, something wrong has been occured")
-        return res.send(_.pick(admin, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate', 'courses']))
+        return res.send(_.pick(admin, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate', 'courses']))
     }
     else {
         let user = await User.findById(req.user._id)
         if (!user) return res.status(404).send("sorry, something wrong has been occured")
-        return res.send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate']))
+        return res.send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate']))
     }
 }
 
@@ -25,10 +25,10 @@ exports.getUser = async (req, res) => {
     if (!user) return res.status(404).send("No user is found with this ID")
     if (user.type === "instructor" || user.type == "admin") {
         user = await user.populate('courses', '-instructor -qa -weeks')
-        return res.send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate', 'courses']))
+        return res.send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate', 'courses']))
     }
     else {
-        return res.send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate']))
+        return res.send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate']))
     }
 }
 
@@ -108,10 +108,10 @@ exports.getUserByEmail = async (req, res) => {
     if (!user) return res.status(404).send("Sorry, we can't find any user with this email or username")
     if (user.type === "instructor" || user.type == "admin") {
         user = await user.populate('courses', '-instructor -qa -weeks')
-        return res.status(200).send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate', 'courses']))
+        return res.status(200).send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate', 'courses']))
     }
     else {
-        return res.status(200).send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lasName', 'type', 'birthDate']))
+        return res.status(200).send(_.pick(user, ['_id', 'userName', 'email', 'firstName', 'lastName', 'type', 'birthDate']))
     }
 }
 
